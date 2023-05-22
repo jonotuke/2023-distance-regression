c873b4069d221759a8e9550bc9926d63b7c53354
Jono Tuke <jonotuke@gmail.com>
2023-05-23 07:41:13 +0930

    Clean up

---
+++ r-code/get_Z.R
@@ -0,0 +1,30 @@
+get_Z_coeff <- function(x1, x2){
+  norm <- sqrt(sum((x1 - x2)^2))
+  (x1 - x2) / norm
+}
+get_Z <- function(pts){
+  n_pts <- length(pts)
+  m <- n_pts * (n_pts - 1) / 2
+  n <- n_pts * 2
+  Z <- matrix(0, nrow = m, ncol = n)
+  row <- 1
+  for(i in 1:(length(pts) - 1)){
+    for(j in (i+1):length(pts)){
+      coef <- get_Z_coeff(pts[[i]], pts[[j]])
+      Z[row, 2*i - 1] <- coef[1]
+      Z[row, 2*i] <- coef[2]
+      Z[row, 2*j - 1] <- -coef[1]
+      Z[row, 2*j] <- -coef[2]
+      row <- row + 1
+    }
+  }
+  Z
+}
+#
+# pts <- list(
+#   x1 = c(0,0),
+#   x2 = c(1,0),
+#   x3 = c(0,1)
+# )
+# get_Z_coeff(x1 = pts$x1, x2 = pts$x2)
+# get_Z(pts)
---
+++ r-code/get_dist.R
@@ -0,0 +1,26 @@
+get_dist <- function(pts){
+  n_pts <- length(pts)
+  n <- n_pts * (n_pts - 1) / 2
+  dist <- numeric(n)
+  index <- 1
+  for(i in 1:(n_pts - 1)){
+    for(j in (i+1):n_pts){
+      dist[index] <- sqrt(sum((pts[[i]] - pts[[j]])^2))
+      index <- index + 1
+    }
+  }
+  dist
+}
+
+pts <- list(
+  x1 = c(0,0),
+  x2 = c(1,0),
+  x3 = c(0,1)
+)
+get_dist(pts)
+
+pts <- tibble(
+  c(0,1,0),
+  c(0,0,1)
+)
+split(pts, seq(3)) |> get_dist()
