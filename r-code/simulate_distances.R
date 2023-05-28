886f8a2240ad65dca2ef635ca3f61daa15e2dae3
Jono Tuke <jonotuke@gmail.com>
2023-05-28 15:54:01 +0930

    Updated the simulate_distance function

--- r-code/simulate_distances.R
+++ r-code/simulate_distances.R
@@ -1,40 +1,47 @@
 simulate_distances <- function(n_pts, sigma_b, beta){
-  # Get data frame of points
-  pts <- tibble(
-    X1 = runif(n_pts, 0, 10),
-    X2 = runif(n_pts, 0, 10),
-    a1 = rnorm(n_pts, mean = 0, sd = sigma_b),
-    a2 = rnorm(n_pts, mean = 0, sd = sigma_b),
-    Y1 = beta * X1 + a1,
-    Y2 = beta * X2 + a2
-  )
-  Dij = pts |> select(X1, X2) |> get_dist_df()
-  Gij = pts |> select(Y1, Y2) |> get_dist_df()
-  Gij_hat = get_linear_model_gij(
-    X = pts |> select(X1, X2),
-    beta = beta,
-    a = pts |> select(a1, a2),
-    Dij = Dij
-  )
+  # X points
+  X <- matrix(runif(n_pts * 2), ncol = 2)
+  colnames(X) <- c("X1", "X2")
+  # Get A
+  A <- matrix(rnorm(n_pts * 2, mean = 0, sd = sigma_b), ncol = 2)
+  colnames(A) <- c("A1", "A2")
+  # Get Y
+  Y <- beta * X + A
+  colnames(Y) <- c("Y1", "Y2")
+  # Get Dij
+  Dij <- cpp_get_dist(X)
+  # Get Gij
+  Gij <- cpp_get_dist(Y)
+  # Get LM Gij
+  LMGij <- get_linear_model_gij(X, beta, A)
+  # Return as a list
   lst <- list()
-  lst$pts <- pts
-  lst$dist <- tibble(
-    Dij, Gij, Gij_hat
-  )
-  lst
+  lst$X <- X
+  lst$A <- A
+  lst$Y <- Y
+  lst$Dij <- Dij
+  lst$Gij <- Gij
+  lst$LMGij <- LMGij
+  return(lst)
 }
-# pacman::p_load(tidyverse, targets)
-# source("r-code/get_dist.R")
-# sims <- simulate_distances(n_pts = 50,
-#                            sigma_b = 0.1, beta = 1)
-# sims$pts
-# sims$pts |>
-#   ggplot(aes(X1, X2)) +
-#   geom_point(aes(fill = "X"), pch = 21) +
-#   geom_point(aes(Y1, Y2, fill = "Y"), pch = 21) +
-#   geom_segment(aes(x = X1, xend = Y1, y = X2, yend = Y2))
-#
-# sims$dist |>
-#   ggplot(aes(Gij, Gij_hat)) +
-#   geom_point()
-#
+pacman::p_load(tidyverse, targets)
+fs::dir_ls("c-code/") |> walk(Rcpp::sourceCpp)
+source("r-code/get_linear_model_gij.R")
+sims <- simulate_distances(
+  n_pts = 200,
+  sigma_b = 0.01, beta = 1
+)
+sims$X |> as_tibble() |>
+  bind_cols(sims$Y) |>
+  ggplot(aes(X1, X2)) +
+  geom_point(aes(fill = "X"), pch = 21) +
+  geom_point(aes(Y1, Y2, fill = "Y"), pch = 21) +
+  geom_segment(aes(x = X1, xend = Y1, y = X2, yend = Y2))
+
+tibble(
+  Gij = sims$Gij,
+  LMGij = sims$LMGij,
+  Dij = sims$Dij
+) |>
+  ggplot(aes(Gij, LMGij, col = cut(Dij, 3))) +
+  geom_point()
